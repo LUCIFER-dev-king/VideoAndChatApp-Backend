c03cb4548ae6690cdf74fe2709e21bddf146f160
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-08-04 22:01:18 +0530

    added mysql

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-
+const db = require("./models");
 app.get("/", (req, res) => {
   res.send("hi");
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running at PORT: ${PORT}`);
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running at PORT: ${PORT}`);
+  });
 });
